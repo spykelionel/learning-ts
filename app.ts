83024ba644a406aa94c162629ed022c475c0ef68
spykelionel <social.lionel@gmail.com>
2023-10-28 21:37:32 +0100

    init file

---
+++ app.ts
@@ -0,0 +1,23 @@
+const a: Number = 3
+
+type User  = {
+    name: String,
+    age: Number
+}
+
+const users: Array<User> = [
+    {
+        name: "Lionel",
+        age: 3
+    },
+    {
+        name: "spyke",
+        age: 3
+    }
+]
+
+const user: User = {
+    name: "Lionel",
+    age: 32
+}
+
